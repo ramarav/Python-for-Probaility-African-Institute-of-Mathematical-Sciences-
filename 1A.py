0c3ecd5eed33daf17d016c6b53265ac051fca824
ramarav <ramarav@gmail.com>
2020-05-07 02:20:26 +0530

    Answer to Q. No. 1A

--- 1A.py
+++ 1A.py
@@ -1,17 +1,4 @@
-#Q. No. 1:
-#1. Suppose you have a fair six-sided die. Now suppose that you throw the die three times. What is the
-#probability that you will get:
-#(a) a 1 on the first throw, a 2 on the second throw, and a 3 on the third throw?
-#(b) a 1, a 2, and a 3 in any order?
-#(c) three sixes?
 
-#Answer :
-#(a)
-#Generally, the probability of obtaining 1 on first through is 1/6 as there are 6 possibilities.
-#Similarly, the possibility of obtaining 2 on second through and 3 on third through is also 1/3 each.
-#If P(A) =P(B) = P(C) = 1/6
-#Then, the total probability P =  P(A) * P(B) * P(C) = 1/6 ³
-#The python code is shown below :
 success = 0
 tries = 0
 for first in range (0,6) :
